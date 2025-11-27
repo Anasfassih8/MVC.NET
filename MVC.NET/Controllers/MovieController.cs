a1166c896249f0b5b92b13fad380b07f849cd57c
anasfassih88@gmail.com <Anasfassih88@gmail.com>
2025-11-27 17:33:04 +0200

    commiting files

---
+++ MVC.NET/Controllers/MovieController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MVC.NET.Controllers
+{
+    public class MovieController:Controller
+    {
+       
+
+        public IActionResult Index() {
+            //ContentResult result = new ContentResult();
+            //result.Content = "Welcome ybasha";
+            //result.ContentType = "text/html";//Default
+            //return result;
+            return Content("Welcome ybasha", "text/html");
+        
+        }
+
+        public IActionResult redirect()
+        {
+            //RedirectResult result = new RedirectResult("https://localhost:44325/Movie/Index");
+            //return result;
+            //return Redirect("https://localhost:44325/Movie/Index");
+            return RedirectToAction("Index");
+        }
+    }
+}
